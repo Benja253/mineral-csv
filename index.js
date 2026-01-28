ebb523541443e34e0fc303e6f649649c47b7dfcb
benja <benja48.9@gmail.com>
2026-01-28 15:54:33 -0300

    mensaje en consola

--- index.js
+++ index.js
@@ -29,3 +29,5 @@ mkdirSync("output", { recursive: true });
 const outFilePath = path.join('output','info_mineral_limpio.csv')
 
 writeFileSync(outFilePath, infoFinal, 'utf-8')
+
+console.log('Se ejecutó todo correctamente. El archivo está en /output/info_minerales_limpio.cvs')
